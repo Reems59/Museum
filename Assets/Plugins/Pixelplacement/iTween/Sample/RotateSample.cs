2f989316da95031759d1b4cb662c4eda39ed8fcc
Remi <reems59242@gmail.com>
2018-03-12 16:59:08 +0100

    MOD - finish step 2 rotate still 3 steps

--- Assets/Plugins/Pixelplacement/iTween/Sample/RotateSample.cs
+++ Assets/Plugins/Pixelplacement/iTween/Sample/RotateSample.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class RotateSample : MonoBehaviour
 {	
-	void Start(){
-		iTween.RotateBy(gameObject, iTween.Hash("x", .25, "easeType", "easeInOutBack", "loopType", "pingPong", "delay", .4));
+	public void rotateBy(GameObject g){
+		iTween.RotateBy(g, new Vector3(0, 0.3f,0), 2f);
 	}
 }
 
